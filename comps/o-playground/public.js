112350b26d6a3b0c5ad2dbeae502dfdf79159c5a
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-02-09 23:12:40 +0800

    feat: Optimize code

---
+++ comps/o-playground/public.js
@@ -0,0 +1,3 @@
+export const prefixContent = `<script src="https://cdn.jsdelivr.net/gh/kirakiray/ofa.js/dist/ofa.min.js#debug"><\/script>
+<link rel="stylesheet" href="https://core.noneos.com/nos-tool/css/theme.css" \/>
+<link rel="stylesheet" href="https://core.noneos.com/nos-tool/css/palette.css" \/>\n`;
